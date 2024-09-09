2b0b9bc3cbe371794866117dceb88191aa878f78
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-09 20:50:52 -0300

    Preparing the interface to use the new conta behaviour

--- src/IConta.java
+++ src/IConta.java
@@ -1,6 +1,8 @@
 public interface IConta {
-    void sacar(double valor);
-    void depositar(double valor);
+    void sacar(double valor, boolean inserirNoHistoricoOperacoes);
+    void depositar(double valor, boolean inserirNoHistoricoOperacoes);
     void transferir(double valor, IConta contaDestino);
     void imprimirExtrato();
+    void inserirOperacao(String nomeOperacao, double valorOperacao);
+    void imprimirHistoricoOperacoes();
 }
